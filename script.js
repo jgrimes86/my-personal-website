3d9757246c56f12526abe2f4a447c7745c404b50
Jim Grimes <jimgrimes86@gmail.com>
2023-08-11 11:23:42 -0400

    buttons work, redoing CSS

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ let i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    const content = this.nextElementSibling;
+    let content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
       content.style.display = "block";
     }
   });
-}
+} 
